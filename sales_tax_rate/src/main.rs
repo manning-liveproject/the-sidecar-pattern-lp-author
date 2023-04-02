49a6ad6b3595478e49a7176b0741dc0b6dc4f636
Michael Yuan <michael@secondstate.io>
2023-04-02 20:54:36 +0000

    Use state store
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- sales_tax_rate/src/main.rs
+++ sales_tax_rate/src/main.rs
@@ -4,6 +4,8 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server};
 use csv::Reader;
 use serde_json::Value;
+use serde_json::json;
+use serde_json::Value::String;
 
 /// This is our service handler. It receives a Request, routes on its
 /// path, and returns a Future of a Response.
@@ -11,7 +13,7 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, anyhow::Er
     match (req.method(), req.uri().path()) {
         // Serve some instructions at /
         (&Method::GET, "/") => Ok(Response::new(Body::from(
-            "Try POSTing data to /find_rate such as: `curl http://localhost:8001/get_rate -XPOST -d '{\"zip\":\"78701\"}'`",
+            "Try POSTing data to /find_rate such as: `curl http://localhost:8001/find_rate -XPOST -d '{\"zip\":\"78701\"}'`",
         ))),
 
         (&Method::POST, "/find_rate") => {
@@ -19,26 +21,20 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, anyhow::Er
             let json: Value = serde_json::from_slice(&byte_stream).unwrap();
             let zip = json["zip"].as_str().unwrap();
 
-            let mut rate = "".to_string();
-            let rates_data: &[u8] = include_bytes!("rates_by_zipcode.csv");
-            let mut rdr = Reader::from_reader(rates_data);
-            for result in rdr.records() {
-                let record = result?;
-                // dbg!("{:?}", record.clone());
-                if zip.eq(&record[0]) {
-                    rate = record[1].to_string();
-                    break;
+            let client = dapr::Dapr::new(3501, "http://localhost".to_string());
+            match client.get_state("statestore", zip).await? {
+                String(rate) => {
+                    dbg!(&rate);
+                    Ok(Response::new(Body::from(rate)))
+                },
+                _ => {
+                    dbg!("Returns an error");
+                    Ok(Response::new(Body::from("Not Found")))
                 }
-            }
-
-            if rate.is_empty() {
-                Ok(Response::new(Body::from("Not Found")))
-            } else {
-                Ok(Response::new(Body::from(rate)))
             }
         }
 
-        // Return the 404 Not Found for other routes.
+        // Return Not Found for other routes.
         _ => {
             Ok(Response::new(Body::from("Not Found")))
         }
@@ -47,6 +43,19 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, anyhow::Er
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Save the sales tax rate for zip code into the Dapr state store.
+    let client = dapr::Dapr::new(3501, "http://localhost".to_string());
+    let rates_data: &[u8] = include_bytes!("rates_by_zipcode.csv");
+    let mut rdr = Reader::from_reader(rates_data);
+    for result in rdr.records() {
+        let record = result?;
+        let kvs = json!([{
+            "key": record[0], "value": record[1]
+        }]);
+        println!("KVS is {}", serde_json::to_string(&kvs)?);
+        client.save_state("statestore", kvs).await?;
+    }
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 8001));
     let make_svc = make_service_fn(|_| {
         async move {
